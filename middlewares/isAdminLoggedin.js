258b22bb0c171700bdf90bd076598b94d2806b1a
Samich786 <msamisignup@gmail.com>
2024-08-30 17:13:44 +0500

    friday

--- middlewares/isAdminLoggedin.js
+++ middlewares/isAdminLoggedin.js
@@ -6,20 +6,21 @@ const JWT_KEY = process.env.JWT_KEY;
 const isAdminLoggedin = async (req, res, next) => {
   try {
     // Extract the token directly
+
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(" ")[1];
 
     if (token) {
       jwt.verify(token, JWT_KEY, async (err, decoded) => {
-        console.log(decoded);
         if (err) {
           console.error("JWT verification error (Owner):", err); // Detailed error logging
           return next(); // Call next to allow checkLoggedIn to try the next middleware
         } else {
           const { id } = decoded;
-          // console.log(id);
+          console.log(id);
           try {
             const owner = await ownerModal.findOne({ _id: id });
+
             if (owner) {
               req.owner = owner;
               return next();
